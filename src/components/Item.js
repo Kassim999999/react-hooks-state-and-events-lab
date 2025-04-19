41fa68304aa310de247aed3faba224fc6945b93e
Kassim <kassimrooney@gmail.com>
2025-04-19 22:52:30 +0300

    Completed Lab

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+
+  const [isInside, setIsInside] = useState(false)
+  const cartClass = isInside ? "InCart" : "OutCart"
+   
+  function handleClick() {
+    setIsInside(prev => !prev)
+  }
+
   return (
-    <li className="">
+    <li className={isInside ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleClick}>{isInside ? "Remove From Cart" : "Add to Cart"}</button>
     </li>
   );
 }
